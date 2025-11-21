32e6b6e9faf09bd50ea8e45a94b0277cab65e0b7
ka.riabtsev <ka.riabtsev@interfax.ru>
2025-11-21 16:06:32 +0300

    add new tests and fixtures

--- niffler-e-2-e-tests-python/tests/test_spending.py
+++ niffler-e-2-e-tests-python/tests/test_spending.py
@@ -117,3 +117,63 @@ def test_total_category_spending_sum(spending_page, clean_spendings_setup):
     spending_page.add_new_spending(amount_2, category, description)
 
     expect(spending_page.category_spending_sum).to_contain_text(f'{amount_1 + amount_2}')
+
+
+@TestData.category(TEST_CATEGORY)
+@TestData.spends({
+    "amount": "101.1",
+    "description": "test_description",
+    "category": {
+        "name": TEST_CATEGORY
+    },
+    "spendDate": "2025-08-08T18:39:27.955Z",
+    "currency": "RUB"
+})
+def test_valid_spending_search(spending_page, clean_spendings_setup, spends, category):
+    spending_page.navigate_to_spending_page()
+
+    spending_page.search_spending(spends['description'])
+
+    expect(spending_page.page.get_by_text('History of Spendings')).to_be_visible()
+    expect(spending_page.spendings_table).to_contain_text(str(spends['amount']))
+    expect(spending_page.spendings_table).to_contain_text(category)
+    expect(spending_page.spendings_table).to_contain_text(spends['description'])
+
+
+@TestData.category(TEST_CATEGORY)
+@TestData.spends({
+    "amount": "101.1",
+    "description": "test_description",
+    "category": {
+        "name": TEST_CATEGORY
+    },
+    "spendDate": "2025-08-08T18:39:27.955Z",
+    "currency": "RUB"
+})
+def test_invalid_spending_search(spending_page, clean_spendings_setup, spends, category):
+    spending_page.navigate_to_spending_page()
+
+    spending_page.search_spending('invalid_search')
+
+    expect(spending_page.page.get_by_text('There are no spendings')).to_be_visible()
+    expect(spending_page.page.get_by_text('History of Spendings')).to_be_visible()
+    expect(spending_page.spendings_table).not_to_contain_text(str(spends['amount']))
+    expect(spending_page.spendings_table).not_to_contain_text(category)
+    expect(spending_page.spendings_table).not_to_contain_text(spends['description'])
+
+
+@TestData.category(TEST_CATEGORY)
+@TestData.spends({
+    "amount": "101.1",
+    "description": "test_description",
+    "category": {
+        "name": TEST_CATEGORY
+    },
+    "spendDate": "2025-08-08T18:39:27.955Z",
+    "currency": "RUB"
+})
+def test_currency_in_amount(spending_page, clean_spendings_setup, spends, category):
+    spending_page.navigate_to_spending_page()
+
+    expect(spending_page.spendings_table).to_contain_text("₽")
+    expect(spending_page.spendings_table).to_contain_text(str(spends['amount']))
