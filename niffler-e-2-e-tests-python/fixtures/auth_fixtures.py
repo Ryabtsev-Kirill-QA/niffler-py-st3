5b07e74fbd8e1d6012d14cf4e49b611295faf47e
ka.riabtsev <ka.riabtsev@interfax.ru>
2025-12-11 22:43:31 +0300

    another place for fixtures

---
+++ niffler-e-2-e-tests-python/fixtures/auth_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+from clients.oauth_client import OAuthClient
+from models.config import Envs
+
+
+@pytest.fixture(scope="session")
+def auth_token(envs: Envs):
+    return OAuthClient(envs).get_token(envs.niffler_username, envs.niffler_password)
