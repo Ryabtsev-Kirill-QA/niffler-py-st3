953a4f712ad1faf4495d8311337a229aea848727
ka.riabtsev <ka.riabtsev@interfax.ru>
2025-12-11 22:40:02 +0300

    fix models

--- niffler-e-2-e-tests-python/databases/spend_db.py
+++ niffler-e-2-e-tests-python/databases/spend_db.py
@@ -1,33 +1,30 @@
 import uuid
-import allure
-from allure_commons.types import AttachmentType
 from typing import Sequence
 from sqlalchemy import create_engine, Engine, event
 from sqlmodel import Session, select
-from models.spend import Category, Spend
+from models.spend import SpendSQL
+from models.category import CategorySQL
+from models.config import Envs
+from utils.allure_helpers import attach_sql
 
 
 class SpendDb:
+    """Клиент для взаимодействия с БД"""
+
     engine: Engine
 
-    def __init__(self, db_url: str):
-        self.engine = create_engine(db_url)
-        event.listen(self.engine, "do_execute", fn=self.attach_sql)
+    def __init__(self, envs: Envs) -> object:
+        self.engine = create_engine(envs.spend_db_url)
+        event.listen(self.engine, "do_execute", fn=attach_sql)
 
-    @staticmethod
-    def attach_sql(cursor, statement, parameters, context):
-        statement_with_params = statement % parameters
-        name = statement.split(" ")[0] + " " + context.engine.url.database
-        allure.attach(statement_with_params, name=name, attachment_type=AttachmentType.TEXT)
-
-    def get_user_categories(self, username: str) -> Sequence[Category]:
+    def get_user_categories(self, username: str) -> Sequence[CategorySQL]:
         with Session(self.engine) as session:
-            statement = select(Category).where(Category.username == username)
+            statement = select(CategorySQL).where(CategorySQL.username == username)
             return session.exec(statement).all()
 
-    def add_user_category(self, username: str, category_name: str) -> Category:
+    def add_user_category(self, username: str, category_name: str) -> CategorySQL:
         with Session(self.engine) as session:
-            new_category = Category(
+            new_category = CategorySQL(
                 id=str(uuid.uuid4()),
                 name=category_name,
                 username=username
@@ -39,27 +36,27 @@ class SpendDb:
 
             return new_category
 
-    def get_category_by_name(self, username: str, category_name: str) -> Category:
+    def get_category_by_name(self, username: str, category_name: str) -> CategorySQL:
         with Session(self.engine) as session:
-            category = select(Category).where(
-                Category.username == username,
-                Category.name == category_name
+            category = select(CategorySQL).where(
+                CategorySQL.username == username,
+                CategorySQL.name == category_name
             )
             return session.exec(category).first()
 
-    def get_category_by_id(self, category_id: str) -> Category:
+    def get_category_by_id(self, category_id: str) -> CategorySQL:
         with Session(self.engine) as session:
-            category = select(Category).where(Category.id == category_id)
+            category = select(CategorySQL).where(CategorySQL.id == category_id)
             return session.exec(category).first()
 
     def delete_category(self, category_id: str):
         with Session(self.engine) as session:
-            category = session.get(Category, category_id)
+            category = session.get(CategorySQL, category_id)
             session.delete(category)
             session.commit()
 
     def get_spend_in_db(self, username: str):
         with Session(self.engine) as session:
-            spend = select(Spend).where(Spend.username == username)
+            spend = select(SpendSQL).where(SpendSQL.username == username)
             result = session.exec(spend).all()
             return result
--- niffler-e-2-e-tests-python/models/spend.py
+++ niffler-e-2-e-tests-python/models/spend.py
@@ -1,31 +1,42 @@
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
+from datetime import datetime
+from models.category import CategorySQL
 
 
-class Category(SQLModel, table=True):
-    id: str = Field(default=None, primary_key=True)
-    name: str
-    username: str
-
-
-class CategoryAdd(BaseModel):
-    name: str
-    username: str | None = None
-    archived: bool | None = None
-
-
-class Spend(SQLModel, table=True):
+class SpendSQL(SQLModel, table=True):
+    __tablename__ = "spend"
     id: str = Field(default=None, primary_key=True)
     username: str
     amount: float
     description: str
-    # spend_date: datetime
+    category_id: str = Field(foreign_key="category.id")
+    spend_date: datetime
     currency: str
+
+
+class Spend(BaseModel):
+    id: str = Field(default=None, primary_key=True)
+    amount: float
+    description: str
+    category: CategorySQL
+    spendDate: datetime
+    currency: str
+    username: str
 
 
 class SpendAdd(BaseModel):
     amount: float
     description: str
-    category: CategoryAdd
+    category: dict
+    spendDate: str
+    currency: str
+
+
+class SpendEdit(BaseModel):
+    id: str = Field(default=None, primary_key=True)
+    amount: float
+    description: str
+    category: dict
     spendDate: str
     currency: str
