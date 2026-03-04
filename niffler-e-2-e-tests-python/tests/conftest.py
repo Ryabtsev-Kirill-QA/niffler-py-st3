d4628e8a8d8eba05441f8c71512d62b7dde427e5
ka.riabtsev <ka.riabtsev@interfax.ru>
2026-03-04 12:33:49 +0300

    delete allure titles

--- niffler-e-2-e-tests-python/tests/conftest.py
+++ niffler-e-2-e-tests-python/tests/conftest.py
@@ -1,19 +1,18 @@
-import allure
 import pytest
 
 
-@allure.title('Добавление категории трат')
 @pytest.fixture(params=[])
 def category(request, spends_client, spend_db):
+    """Добавление категории трат"""
     category_name = request.param
     category = spends_client.add_category(category_name)
     yield category.name
     spend_db.delete_category(category.id)
 
 
-@allure.title('Добавление траты')
 @pytest.fixture(params=[])
 def spends(request, spends_client):
+    """Добавление траты"""
     spend = spends_client.add_spends(request.param)
     yield spend
     all_spends = spends_client.get_spends()
@@ -21,9 +20,9 @@ def spends(request, spends_client):
         spends_client.remove_spends([spend.id])
 
 
-@allure.title('Удаление всех трат до и после теста')
 @pytest.fixture(scope="function")
 def clean_spendings_setup(spends_client):
+    """Удаление всех трат до и после теста"""
     all_spendings = spends_client.get_spends()
     spending_ids = [spending.id for spending in all_spendings]
     if spending_ids:
@@ -37,9 +36,9 @@ def clean_spendings_setup(spends_client):
         spends_client.remove_spends(spending_ids)
 
 
-@allure.title('Удаление всех категорий после теста')
 @pytest.fixture(scope="function")
 def clean_categories(spends_client, spend_db):
+    """Удаление всех категорий после теста"""
     yield
     all_categories = spends_client.get_categories()
     categories_ids = [category.id for category in all_categories]
